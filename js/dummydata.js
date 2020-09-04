1f1207e898b9909b50d8461a8dc8a1d4aa5312cf
Lasse Lenting <lasselenting@gmail.com>
2020-09-04 16:09:45 +0200

    Update 0.0.1

---
+++ js/dummydata.js
@@ -0,0 +1,24 @@
+// grid dummy data
+var dummyData = {
+	"Helmen":"<li class=\"product\"><a target=\"_blank\" href=\"https://partner.bol.com/click/click?p=2&t=url&s=1102409&f=TXL&url=https%3A%2F%2Fwww.bol.com%2Fnl%2Fp%2Fhelm-vito-jet-palermo-mat-zwart-s-scooter-motor%2F9200000101287515%2F&name=HELM%20VITO%20JET%20PALERMO%20MAT%20ZWART%20S%20Scooter%20%26%20Motor\"><img src=\"https://media.s-bol.com/BBgL4w4ky67Q/550x424.jpg\"></a><p class=\"pricename\">Helm Vito </p><p class=\"dot\">&nbsp;·</p><p class=\"price\">&nbsp;€43,95</p><p class=\"dot\">&nbsp;- Bol.com</p></li><li class=\"product\"><a target=\"_blank\" href=\"https://partner.bol.com/click/click?p=2&t=url&s=1102409&f=TXL&url=https%3A%2F%2Fwww.bol.com%2Fnl%2Fp%2Fvinz-santiago-systeemhelm-motorhelm-mat-zwart-maat-m%2F9200000121306960%2F&name=Vinz%20Santiago%20Systeemhelm%20-%20Motorhelm%20-%20Mat%20Zw...\"><img src=\"https://media.s-bol.com/Ro9Ao94wNw9R/550x595.jpg\"></a><p class=\"pricename\">Vinz Helm </p><p class=\"dot\">&nbsp;·</p><p class=\"price\">&nbsp;€82,95</p><p class=\"dot\">&nbsp;- Bol.com</p></li>",
+	"[Item-2]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-3]":"<li class=\"product\"></li>",
+	"[Item-4]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-5]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-6]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-7]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-8]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-9]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-10]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-11]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-12]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-13]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-14]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-15]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-16]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-17]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-18]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-19]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-20]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>",
+	"[Item-21]":"<li class=\"product\"><img src=\"https://www.buromac.com/sites/default/files/public/styles/product_large/public/images/product/be-nl/650901_1.jpg?itok=t88TaLRJ\"></li>"
+}
